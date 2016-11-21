6bc2fc30707902055a65077d8ac302d0de9d789a
eli.boey <eli.boey@student.ehb.be>
2016-11-21 22:08:17 +0100

    Quick fix van kamiel

--- public/js/storeTeam.js
+++ public/js/storeTeam.js
@@ -9,11 +9,11 @@ $(function(){
 
         var gameID = $("#inputGameID").val();
         var teammembers = $("#game"+gameID+"players").val();
-
+        
         if(teammembers!=1) {
             var html = '<div class="form-group row"><label for="teammembers" class="col-sm-2 col-form-label">Teammembers</label><div class="col-sm-10" id="teammembers"></div>';
 
-            $("#submit").before(html);
+            $("#submitbutton").before(html);
 
             for (var i = 0; i < teammembers; i++) {
                 var html2 = "<input type='text' class='form-control' placeholder='E-mail' name='teammembers[]'>";
@@ -27,4 +27,3 @@ $(function(){
     });
 
 });
-
