e4fc339c5b571d31ddffa0baf2910e286da7b005
Nico <lostitz0r@gmail.com>
2016-11-21 17:09:02 +0100

    uw dikke tante

--- resources/views/registration/show.blade.php
+++ resources/views/registration/show.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.master')
 
 @section('content')
-    <li>{{ $user->id }}</li>
-    <li>{{ $user->firstName }}</li>
-    <li>{{ $user->lastName }}</li>
-    <li>{{ $user->email }}</li>
-    @if(isset($team))
-      {{ 'kben een mongool' }}
-      <li>{{ $team->name }}</li>
-    @endif
+  <li>{{ $user->id }}</li>
+  <li>{{ $user->firstName }}</li>
+  <li>{{ $user->lastName }}</li>
+  <li>{{ $user->email }}</li>
+  @if(isset($team))
+    {{ 'kben een mongool' }}
+    <li>{{ $team->name }}</li>
+  @endif
 @stop
 
 @section('scripts')
