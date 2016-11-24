f7fde8caa197eccf067506d4623013fcfdf60f26
LoStIt <lostitz0r@gmail.com>
2016-11-24 18:02:25 +0100

    Sync van laptop na pc ff derpen

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,76 +1,91 @@
-@extends('layouts.app')
+@extends('layouts.register')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-                        {{ csrf_field() }}
+    <div class="col-md-10 col-md-offset-1">
+        <form method="POST" action="storeteam" id="registerteamform" class="group">
+            {{ csrf_field() }}
+            <center><img src="img/logo.png" alt="logo" id="logo"></center>
+            <h2>inschrijving</h2>
+            <div class="line"></div>
+            <div class="left">
+                <div class="form-group row {{ $errors->has('email') ? ' has-error' : '' }}">
+                    <div class="col-sm-10">
+                        <input type="email" name="email" class="form-control" id="inputEmail" placeholder="E-mail" required autofocus />
+                    </div>
+                </div>
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                <div class="form-group row {{ $errors->has('firstname') ? ' has-error' : '' }}">
+                    <div class="col-sm-10">
+                        <input type="text" name="firstname" class="form-control" id="inputFirstName" placeholder="Voornaam" required />
+                    </div>
+                </div>
+                @if ($errors->has('firstname'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('firstname') }}</strong>
+                    </span>
+                @endif
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                <div class="form-group row {{ $errors->has('lastname') ? ' has-error' : '' }}">
+                    <div class="col-sm-10">
+                        <input type="text" name="lastname" class="form-control" id="inputLastName" placeholder="Achternaam" required />
+                    </div>
+                </div>
+                @if ($errors->has('lastname'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('lastname') }}</strong>
+                    </span>
+                @endif
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <input type="password" name="password" class="form-control" id="inputPassword" placeholder="Wachtwoord" required />
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <input type="password" name="password_confirmation" class="form-control" id="inputVerifyPassword" placeholder="Herhaal je wachtwoord" required>
+                    </div>
+                </div>
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <select name="gameid" id="inputGameID" class="form-control">
+                            <option value="">Games</option>
+                        </select>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <div class="right">
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <input type="text" name="teamname" class="form-control" id="inputTeamName" placeholder="Teamnaam">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <div class="col-sm-10" id="teammembers">
+                        <input type="text" class="form-control" placeholder="E-mail" name="teammembers[]">
+                        <input type="text" class="form-control" placeholder="E-mail" name="teammembers[]">
+                        <input type="text" class="form-control" placeholder="E-mail" name="teammembers[]">
+                        <input type="text" class="form-control" placeholder="E-mail" name="teammembers[]">
+                        <input type="text" class="form-control" placeholder="E-mail" name="teammembers[]">
+                    </div>
+                </div>
+
+            </div>
+
+            <input type="submit" name="submitbutton" id="submitbutton" class="btn btn-primary" />
+        </form>
     </div>
-</div>
-@endsection
+@stop
