fe0c0cc2f2da1dfc87c2abdeb03804d6f65e89b4
kamielk <kamielklumpers@gmail.com>
2016-11-28 18:19:28 +0100

    inschrijven als team view fixed

--- resources/views/registration/create.blade.php
+++ resources/views/registration/create.blade.php
@@ -12,150 +12,146 @@
         </div>
     @endif
 
+    @if(Session::has('err'))
+        <div class="alert alert-danger">
+            {{ Session::get('err') }}
+        </div>
+    @endif
+    <br/>
+
     <form class="form-horizontal" method="POST" action="storeteam" id="registerteamform">
         {{ csrf_field() }}
         @foreach(App\Game::all() as $game)
             <input type="hidden" id="game{{$game->id}}players" value="{{$game->maxPlayers}}">
         @endforeach
+
         <h2>inschrijven als team</h2>
         <div class="line"></div>
 
-        <div class="left">
-            <div class="form-group row {{ $errors->has('email') ? ' has-error' : '' }}">
-                <div class="col-sm-10">
-                    <input type="email" name="email" class="form-control" id="inputEmail" placeholder="E-mail" required autofocus />
-                </div>
+        {{-- email --}}
+        <div class="form-group row {{ $errors->has('email') ? ' has-error' : '' }}">
+            <label for="inputEmail" class="col-md-4 control-label">E-mail</label>
+            <div class="col-md-4">
+                <input type="email" name="email" class="form-control" id="inputEmail" placeholder="E-mail" required autofocus value="{{old('email')}}"/>
             </div>
             @if ($errors->has('email'))
                 <span class="help-block">
                     <strong>{{ $errors->first('email') }}</strong>
                 </span>
             @endif
-            <div class="form-group row {{ $errors->has('firstname') ? ' has-error' : '' }}">
-                <div class="col-sm-10">
-                    <input type="text" name="firstname" class="form-control" id="inputFirstName" placeholder="Voornaam" required />
-                </div>
+        </div>
+
+        {{-- reminder email --}}
+        <div class="form-group row {{ $errors->has('email') ? ' has-error' : '' }}">
+            <label for="inputReminderEmail" class="col-md-4 control-label">Reminder E-mail (Deze mail wordt gebruikt voor nieuwsbrieven en is optioneel)</label>
+            <div class="col-md-4">
+                <input type="text" value="{{ old('reminderemail') }}" name="reminderemail" class="form-control" id="inputReminderEmail" placeholder="Reminder E-mail">
             </div>
-            @if ($errors->has('firstname'))
-                <span class="help-block">
+        </div>
+
+        {{-- firstname --}}
+        <div class="form-group row {{ $errors->has('firstname') ? ' has-error' : '' }}">
+            <label for="inputReminderEmail" class="col-md-4 control-label">Voornaam</label>
+            <div class="col-md-4">
+                <input type="text" value="{{old('firstname')}}" name="firstname" class="form-control" id="inputFirstName" placeholder="Voornaam" required/>
+            </div>
+        </div>
+        @if ($errors->has('firstname'))
+            <span class="help-block">
                     <strong>{{ $errors->first('firstname') }}</strong>
                 </span>
-            @endif
+        @endif
 
-            <div class="form-group row {{ $errors->has('lastname') ? ' has-error' : '' }}">
-                <div class="col-sm-10">
-                    <input type="text" name="lastname" class="form-control" id="inputLastName" placeholder="Achternaam" required />
-                </div>
+        {{-- lastname --}}
+        <div class="form-group row {{ $errors->has('lastname') ? ' has-error' : '' }}">
+            <label for="inputReminderEmail" class="col-md-4 control-label">Achternaam</label>
+            <div class="col-md-4">
+                <input type="text" name="lastname" class="form-control" id="inputLastName" placeholder="Achternaam" required />
             </div>
-            @if ($errors->has('lastname'))
-                <span class="help-block">
+        </div>
+        @if ($errors->has('lastname'))
+            <span class="help-block">
                     <strong>{{ $errors->first('lastname') }}</strong>
                 </span>
-            @endif
+        @endif
 
-            <div class="form-group row">
-                <div class="col-sm-10">
-                    <input type="password" name="password" class="form-control" id="inputPassword" placeholder="Wachtwoord" required />
-                </div>
-            </div>
-        </div>
-
-        {{-- Activities --}}
-        <div class="col-md-4">
-            <h3>Activiteiten</h3>
-        </div>
-        @foreach($activities as $activity)
-            <div class="form-group row">
-                <div class="checkbox col-md-4">
-                    <label><input type="checkbox" name="activities[]"value="{{$activity->id}}">{{$activity->name}} - Places left: {{$activity->maxUsers - $activity->users->count()}}</label>
-                </div>
-            </div>
-        @endforeach
-
-        {{-- options --}}
-        <div class="col-md-4">
-            <h3>Extra Opties</h3>
-        </div>
-        @foreach($options as $option)
-            <div class="form-group row">
-                <div class="checkbox col-md-4">
-                    <label><input type="checkbox" name="options[]"value="{{$option->id}}">{{$option->name}} - Price: €{{ number_format($option->price,2) }}</label>
-                </div>
-            </div>
-        @endforeach
-
+        {{-- wachtwoord --}}
         <div class="form-group row">
-            <label for="inputGameID" class="col-sm-2 col-form-label">Game</label>
-            <div class="col-sm-10">
-                <select name="gameid" id="inputGameID" class="form-control">
-                    @foreach(App\Game::all() as $game)
-                        <option value={{$game->id}}>{{$game->name}}</option>
-                    @endforeach
-                </select>
-            <div class="form-group row">
-                <div class="col-sm-10">
-                    <input type="password" name="password_confirmation" class="form-control" id="inputVerifyPassword" placeholder="Herhaal je wachtwoord" required>
-                </div>
-            </div>
-            @if ($errors->has('password'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('password') }}</strong>
-                </span>
-            @endif
-
-            <div class="form-group row">
-                <div class="col-sm-10">
-                    <select name="gameid" id="inputGameID" class="form-control">
-                        @foreach(App\Game::all() as $game)
-                            <option value={{$game->id}}>{{$game->name}}</option>
-                        @endforeach
-                    </select>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="right">
-            <div class="form-group row">
-                <div class="col-sm-10">
-                    <input type="text" name="teamname" class="form-control" id="inputTeamName" placeholder="Teamnaam">
-                </div>
-            </div>
-
-            <div class="form-group row">
-                <div class="col-sm-10" id="teammembers">
-                    <input type="text" class="form-control" placeholder="E-mail" name="teammembers[]">
-                    <input type="text" class="form-control" placeholder="E-mail" name="teammembers[]">
-                    <input type="text" class="form-control" placeholder="E-mail" name="teammembers[]">
-                    <input type="text" class="form-control" placeholder="E-mail" name="teammembers[]">
-                    <input type="text" class="form-control" placeholder="E-mail" name="teammembers[]">
-                </div>
+            <label for="inputReminderEmail" class="col-md-4 control-label">Wachtwoord</label>
+            <div class="col-md-4">
+                <input type="password" name="password" class="form-control" id="inputPassword" placeholder="Wachtwoord" required />
             </div>
         </div>
+
+        {{-- wachtwoord check --}}
+        <div class="form-group row">
+            <label for="inputReminderEmail" class="col-md-4 control-label">Wachtwoordverificatie</label>
+            <div class="col-md-4">
+                <input type="password" name="password" class="form-control" id="inputPassword" placeholder="Wachtwoordverificatie" required />
+            </div>
+        </div>
+
+        {{-- activities --}}
         <div class="col-md-4">
             <h3>Activiteiten</h3>
         </div>
         <div class="form-group row">
             @foreach($activities as $activity)
-                <div class="checkbox col-md-4">
+                <div class="checkbox col-md-6">
                     <label><input type="checkbox" name="activities[]"value="{{$activity->id}}">{{$activity->name}} - Places left: {{$activity->maxUsers - $activity->users->count()}}</label>
                 </div>
             @endforeach
         </div>
 
+        {{-- options --}}
         <div class="col-md-4">
-            <h3>Extra's</h3>
+            <h3>Extra Opties</h3>
         </div>
         <div class="form-group row">
             @foreach($options as $option)
-                <div class="checkbox col-md-6">
+                <div class="checkbox col-md-4">
                     <label><input type="checkbox" name="options[]"value="{{$option->id}}">{{$option->name}} - Price: €{{ number_format($option->price,2) }}</label>
                 </div>
             @endforeach
         </div>
 
+        <div class="col-md-4">
+            <h3>Game</h3>
+        </div>
+        <div class="form-group row">
+            <div class="col-md-4">
+                <select name="gameid" id="inputGameID" class="form-control">
+                    @foreach(App\Game::all() as $game)
+                        <option value={{$game->id}}>{{$game->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+
+
+        <div class="form-group row">
+            <div class="col-md-4">
+                <h3>Team</h3>
+            </div>
+
+            <div class="col-md-4">
+                <input type="text" name="teamname" class="form-control" id="inputTeamName" placeholder="Teamnaam">
+                <div id="members">
+                    <div id="team">
+                        <input type="text" class="form-control" placeholder="E-mail" name="teammembers[]">
+                        <input type="text" class="form-control" placeholder="E-mail" name="teammembers[]">
+                        <input type="text" class="form-control" placeholder="E-mail" name="teammembers[]">
+                        <input type="text" class="form-control" placeholder="E-mail" name="teammembers[]">
+                        <input type="text" class="form-control" placeholder="E-mail" name="teammembers[]">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
         <button id="submitbutton" name="submitbtton" type="button" class="btn btn-primary">Submit</button>
     </form>
+
 @stop
 
 @section('scripts')
