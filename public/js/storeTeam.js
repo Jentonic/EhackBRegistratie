0b108472c64b918971fe61ace677c4f9a3a6b048
kamielk <kamielklumpers@gmail.com>
2016-11-30 08:50:53 +0100

    FIX team inschrijven

--- public/js/storeTeam.js
+++ public/js/storeTeam.js
@@ -2,20 +2,25 @@
  * Created by Kamiel Klumpers on 20/11/2016.
  */
 
+function updateView(){
+    $("#members").html('');
+
+    var gameID = $("#inputGameID").val();
+    var teammembers = $("#game"+gameID+"players").val();
+
+    if(teammembers!=1) {
+        for (var i = 0; i < teammembers-1; i++) {
+            var html2 = "<input type='text' class='form-control' placeholder='E-mail' name='teammembers[]'>";
+            $("#members").append(html2);
+        }
+    }
+}
+
 $(function(){
+    updateView();
 
     $("#inputGameID").change(function(){
-        $("#members").html('');
-
-        var gameID = $("#inputGameID").val();
-        var teammembers = $("#game"+gameID+"players").val();
-        
-        if(teammembers!=1) {
-            for (var i = 0; i < teammembers-1; i++) {
-                var html2 = "<input type='text' class='form-control' placeholder='E-mail' name='teammembers[]'>";
-                $("#members").append(html2);
-            }
-        }
+        updateView()
     });
 
     $("#submitbutton").click(function(){
