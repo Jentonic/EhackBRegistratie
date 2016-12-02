3404f115596736f4f9954fa4f331e68ebbeeeaf5
Kamiel Klumpers <kamielk@MacBook-Pro-van-Kamiel.local>
2016-12-02 18:54:25 +0100

    volzet change

--- app/Game.php
+++ app/Game.php
@@ -9,4 +9,10 @@ class Game extends Model
   public function teams(){
     return $this->hasMany('App\Team','gameID');
   }
+
+  public function isFull(){
+      $max = $this->maxTeams;
+      $now = $this->teams()->count();
+      return $now>=$max;
+  }
 }
