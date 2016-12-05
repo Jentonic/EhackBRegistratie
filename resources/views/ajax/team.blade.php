22b1f5e961b11e174ea53c4a730557e92ec44171
eli.boey <eli.boey@student.ehb.be>
2016-12-05 14:11:37 +0100

    mail naar teamleader button

--- resources/views/ajax/team.blade.php
+++ resources/views/ajax/team.blade.php
@@ -3,7 +3,7 @@
   <div class="form-group row checkbox">
       <label>
         <input type="radio" name="team" value="{{$team->id}}">
-         {{$team->name}} - Plaatsen: {{ ($team->game->maxPlayers) - $team->invites()->count() - $team->users()->count() }} - <a href="mailto:{{ $team->leader->email }}" target="_top">Mail naar de teamleader</a>
+         {{$team->name}} - Plaatsen: {{ ($team->game->maxPlayers) - $team->invites()->count() - $team->users()->count() }} - <br/><a class="btn btn-info" href="mailto:{{ $team->leader->email }}" target="_top">Mail naar de teamleader</a>
       </label>
   </div>
 @endforeach
